66a6e47b064123d5a8a23e02651930c64b8dd20d
David Banks <dave@hoglet.com>
2022-06-20 12:52:13 +0100

    Test/Positron: add other files

---
+++ test/positron/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Log all memory reads/writes/errors
+# Log all samples
+# Ignore the LIC input
+# Output address, hex, instruction, state, cycles
+
+COMMON="--mem=FFF -d1 --lic= -ahisy"
+
+./posigrab posiBoot0xFFFE8k.fixed posiBoot0xFFFE8k.bin
+./posigrab posiBootF105.txt posiBootF105.bin
+
+../../decode6809 $COMMON                                         posiBoot0xFFFE8k.bin > posiBoot0xFFFE8k.log
+../../decode6809 $COMMON --ba= --bs= --reg_pc=F105 --reg_s=0900      posiBootF105.bin >     posiBootF105.log
