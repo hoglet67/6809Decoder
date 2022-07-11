0fe904de42f117e76d091840412c294621cf5902
David Banks <dave@hoglet.com>
2022-07-11 16:41:01 +0100

    Tests: rename and update build.sh

--- test/assembler/build.sh
+++ test/assembler/build.sh
@@ -10,5 +10,14 @@ do
     echo $i
     bin=${i%.asm}
     bin=${bin^^}
+    lst=${bin}.lst
+    asm6809 -l $lst $i > $DIR/$bin
+done
+
+for i in `ls *.bas`
+do
+    echo $i
+    bin=${i%.bas}
+    bin=${bin^^}
     basictool -t -r $i > $DIR/$bin
 done
