a8bdc4148d9a8fc1007cf887213659583d401f13
David Banks <dave@hoglet.com>
2022-07-21 17:21:20 +0100

    Test: bug fixes to random test case

--- test/assembler/random.asm
+++ test/assembler/random.asm
@@ -5,20 +5,34 @@ ZP_RND_WA EQU $10
 ZP_FP_TMP EQU $20
 TEST      EQU $7E00
 
-BEGIN  BRA   RESET
+BEGIN
 
 RESET  LDS   #$200
+       ORCC  #$50
+
+       ;; Disable all interrupts
+       LDA   #$7F
+       STA   $FE4E
+       STA   $FE6E
+
+       ;; Page in ROM 0
+       LDA   #$00
+       STA   $FE30
+
+       ;; Write protect 8000-FFFF
+       LDA   $FE31
+       ORA   #$11
+       STA   $FE31
 
        JSR   ctrInc
-
 
        ;; Seed the random number generator from VIA T1C
        LDD   $fe44
        STD   ZP_RND_WA
-       LDD   $fe44
-       STD   ZP_RND_WA + 2
-       LDA   $fe44
-       STA   ZP_RND_WA + 4
+;;       LDD   $fe44
+;;       STD   ZP_RND_WA + 2
+;;       LDA   $fe44
+;;       STA   ZP_RND_WA + 4
 
        LDX  #TEST
 writeLoop
@@ -31,10 +45,10 @@ writeLoop
        STA  ,X+
        LDA  ZP_RND_WA + 3
        STA  ,X+
-       CMPA #TEST+$200
+       CMPX #TEST+$80
        BNE  writeLoop
 
-       JMP  TEST+$100
+       JMP  TEST
 
        ;; 32-bit PRBS from BBC Basic
 rndNext
@@ -85,26 +99,49 @@ ctrLoop
 ctrDone
       RTS
 
+
+ILL_HANDLER
+      RTI
+SWI3_HANDLER
+      RTI
+SWI2_HANDLER
+      RTI
+FIRQ_HANDLER
+      RTI
+IRQ_HANDLER
+      PSHS A
+      LDA #$7F
+      STA $FE4D
+      STA $FE6D
+      PULS A
+      RTI
+SWI_HANDLER
+      RTI
+NMI_HANDLER
+      JMP RESET
+RST_HANDLER
+      JMP RESET
+
       ORG $F7F0
 
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
+      FDB ILL_HANDLER
+      FDB SWI3_HANDLER
+      FDB SWI2_HANDLER
+      FDB FIRQ_HANDLER
+      FDB IRQ_HANDLER
+      FDB SWI_HANDLER
+      FDB NMI_HANDLER
+      FDB RST_HANDLER
 
       ORG $FFF0
 
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
-      FDB RESET
+      FDB ILL_HANDLER
+      FDB SWI3_HANDLER
+      FDB SWI2_HANDLER
+      FDB FIRQ_HANDLER
+      FDB IRQ_HANDLER
+      FDB SWI_HANDLER
+      FDB NMI_HANDLER
+      FDB RST_HANDLER
 
       END BEGIN
