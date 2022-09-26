4ab491d6f2c044e28e6e583aa6ae220d4fafaee0
David Banks <dave@hoglet.com>
2022-09-26 10:29:07 +0100

    Test: Added posiBootDA15 sample

--- test/positron/run.sh
+++ test/positron/run.sh
@@ -9,6 +9,8 @@ COMMON="--mem=FFF -d1 --lic= -ahisy"
 
 ./posigrab posiBoot0xFFFE8k.fixed posiBoot0xFFFE8k.bin
 ./posigrab posiBootF105.txt posiBootF105.bin
+./posigrab posiBootDA15.txt posiBootDA15.bin
 
 ../../decode6809 $COMMON                                         posiBoot0xFFFE8k.bin > posiBoot0xFFFE8k.log
 ../../decode6809 $COMMON --ba= --bs= --reg_pc=F105 --reg_s=0900      posiBootF105.bin >     posiBootF105.log
+../../decode6809 $COMMON                                             posiBootDA15.bin >     posiBootDA15.log
